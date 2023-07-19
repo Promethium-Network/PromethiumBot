76042561e8caa5eaf7cb147dc48b8a7252e43419
Shradinx <luphcore@gmail.com>
2023-07-19 19:23:32 -0400

    reformat minigames info

--- PromethiumBot.py
+++ PromethiumBot.py
@@ -64,8 +64,7 @@ async def serverinfo(ctx: SlashContext):
         value=f"Our current gamemodes, as of {date}, include Earth and Minigames. "
         "\n \n Earth features a 1:1000 scale map of planet Earth, and includes functionality for things such as towns and nations, a fully "
         "\n functioning chest shop in the spawn, and a variety of vehicles including: cars, bikes, planes, helicopters, submarines, and many more!"
-        "\n \n Minigames features a variety of minigames, both solo and multiplayer, that you can play! These games include BlockHunt, Dropper, "
-        "\n Chess, and Zombies!"
+        "\n \n Minigames features a variety of minigames, both solo and multiplayer, that you can play! These games include BlockHunt, Dropper, Chess, and Zombies!"
     )
 
     # EmbedFields for networkLinks Embed
