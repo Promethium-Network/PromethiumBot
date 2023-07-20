f426492696095ef703ea7cd0174b995031c26824
Shradinx <luphcore@gmail.com>
2023-07-19 20:06:58 -0400

    transcript forwarder complete

--- transcriptForwarder.py
+++ transcriptForwarder.py
@@ -24,6 +24,7 @@ class transcriptForwarder(interactions.Extension):
     # Print confirmation message
     print("Extension Loaded!")
 
+    '''
     # Define /forward slash command
     @interactions.slash_command(
         name="forward",
@@ -31,18 +32,31 @@ class transcriptForwarder(interactions.Extension):
     )
     # Define function for /forward
     async def forward(self, ctx: interactions.SlashContext):
-        # Fetch #ticket-transcripts channel from public discord
-        channel = await self.client.fetch_channel(1125955352198926458)
+        # Fetch channels from public and staff discord
+        ticketChannel = await self.client.fetch_channel(1125955352198926458)
+        transcriptBackupChannel = await self.client.fetch_channel(1126144654950269001)
 
         # Get full channel history, then flatten it from async iterator to list
-        history = await channel.history().flatten()
+        history = await ticketChannel.history().flatten()
 
         # For every message in the channel's history, if the author is Ticket Tool, print the embed in the message
         for message in history:
             if message.author.id == 557628352828014614:
-                print(message.embeds[0])
+                await transcriptBackupChannel.send(f"{message.attachments[0].url}", embeds=message.embeds[0])
+                await ctx.send("Transcript copied to Staff Discord!")
                 break
-        await ctx.send('shit')
+    '''
+
+    @interactions.listen()
+    async def on_message_create(self, event: interactions.Message):
+        if event.message.author.id == 557628352828014614 and event.message.channel == 1125955352198926458:
+            # Fetch channels from public and staff discord
+            ticketChannel = await self.client.fetch_channel(1125955352198926458)
+            transcriptBackupChannel = await self.client.fetch_channel(1126144654950269001)
+            botTesting = await self.client.fetch_channel(1130893800357773312)
+
+            await transcriptBackupChannel.send(f"{event.message.attachments[0].url}", embeds=event.message.embeds[0])
+            await botTesting.send("Transcript copied to Staff Discord!")
 
 
 def setup(client):
