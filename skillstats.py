85659052791e39a4e17ab3a7685fa21a57d09b1d
Shradinx <luphcore@gmail.com>
2023-08-18 11:19:27 -0400

    add comments
    add comments to skillstats

--- skillstats.py
+++ skillstats.py
@@ -1,16 +1,25 @@
+# Name: skillstats.py
+# Parent File: main.py
+# Date: 8-18-23
+# Author: Shradinx
+# -------------------------------
+
 import interactions
 import mysql.connector as db_connector
 import os
 from mojang import API
 
+# moblang api
 api = API()
 
 hostname = os.environ.get("DB_HOST")
 username = os.environ.get("DB_SKILLS_USER")
 password = os.environ.get("DB_SKILLS_PASS")
 dbName = os.environ.get("DB_SKILLS_NAME")
+
 statement = "SELECT ID, AGILITY_LEVEL, ENDURANCE_LEVEL, ENCHANTING_LEVEL, EXCAVATION_LEVEL, FARMING_LEVEL, FISHING_LEVEL, FORAGING_LEVEL, MINING_LEVEL, FORGING_LEVEL, AGILITY_XP, ENDURANCE_XP, ENCHANTING_XP, EXCAVATION_XP, FARMING_XP, FISHING_XP, FORAGING_XP, MINING_XP, FORGING_XP FROM s9_skills.SkillData"
 playerList = []
+
 skillNameList = [
     interactions.SlashCommandChoice(name="Agility", value="AGILITY_LEVEL"),
     interactions.SlashCommandChoice(name="Endurance", value="ENDURANCE_LEVEL"),
@@ -30,18 +39,23 @@ levelIndex = 0
 skillRowIndex = 0
 skillName = ""
 
+# connect to skills database
 db = db_connector.connect(
     host=hostname,
     user=username,
     password=password,
     database=dbName,
 )
+# execute statement and fetch the results
 cursor = db.cursor()
 cursor.execute(statement)
 results = cursor.fetchall()
 for i in range(len(results)):
+    # get the user uuid
     uuid = results[i][0]
+    # convert to minecraft username
     minecraftUser = api.get_username(uuid=uuid)
+    # add slash command choice to player list with username
     playerList.append(interactions.SlashCommandChoice(
         name=minecraftUser, value=minecraftUser))
 
@@ -111,15 +125,21 @@ class SkillStats(interactions.Extension):
                 skillName = "Forging"
 
         for i in range(len(results)):
+            # get user uuid
             uuid = results[i][0]
+            # add the skill level to the skill level list
             skillLevelList.append(results[i][levelIndex])
+            # add the xp amount to the skill xp list
             skillXPList.append(results[i][xpIndex])
             if api.get_username(uuid) == user:
+                # set the level and xp index to i
                 skillLevelIndex, skillXPIndex = i, i
                 break
 
+        # build the embed fields
         nameField = interactions.EmbedField(name="Player Name", value=f"{user}")
         levelField = interactions.EmbedField(name="Skill Level", value=f"{skillLevelList[skillLevelIndex]}")
         xpField = interactions.EmbedField(name="Skill XP", value=f"{skillXPList[skillXPIndex]}")
+        # build the actual embed
         skillEmbed = interactions.Embed(title=f"{skillName} Skill Info", description=" ", fields=[nameField, levelField, xpField], color="#991aed")
         await ctx.send(embed=[skillEmbed])
